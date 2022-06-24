821007f4397531ff3c6fc3cc80c43a6868230381
Nick Friday <nfriday@yandex.ru>
2022-06-24 16:09:01 +0300

    hop: refined

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -1,11 +1,31 @@
 local hop = require('hop')
-hop.setup({})
+hop.setup({ keys = 'fjdksla;gheiworuxvmc' })
 
 local mx = require('mapx')
-mx.nnoremap('s', '<NOP>')
-mx.nnoremap('sl', function() hop.hint_words() end)
-mx.nnoremap('sh', function() hop.hint_words() end)
-mx.nnoremap('sj', function() hop.hint_lines() end)
-mx.nnoremap('sk', function() hop.hint_lines() end)
-mx.nnoremap('sf', function() hop.hint_char1() end)
-mx.nnoremap('ss', function() hop.hint_char2() end)
+mx.nnoremap('s', '<Nop>')
+mx.vnoremap('s', '<Nop>')
+mx.onoremap('<leader>', '<Nop>')
+mx.nname('s', 'hop')
+mx.vname('s', 'hop')
+mx.oname('<leader>', 'hop')
+local function map(key, action, description)
+  mx.nnoremap('s' .. key, function() action() end, description)
+  mx.vnoremap('s' .. key, function() action() end, description)
+  mx.onoremap('<leader>' .. key, function() action() end, description)
+end
+
+local dir = require('hop.hint').HintDirection
+
+map('w', hop.hint_words, 'words')
+map('l', function() hop.hint_words({ direction = dir.AFTER_CURSOR }) end,
+  'words forward')
+map('h', function() hop.hint_words({ direction = dir.BEFORE_CURSOR }) end,
+  'words backward')
+map('g', hop.hint_lines, 'lines')
+map('j', function() hop.hint_lines({ direction = dir.AFTER_CURSOR }) end,
+  'lines forward')
+map('k', function() hop.hint_lines({ direction = dir.BEFORE_CURSOR }) end,
+  'lines backward')
+map('f', hop.hint_char1, '1 char')
+map('s', hop.hint_char2, '2 chars')
+map('/', hop.hint_patterns, 'pattern')
