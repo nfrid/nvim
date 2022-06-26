e6e7eaac3d96f3264da6abc81d21f9f09f634e75
Nick Friday <nfriday@yandex.ru>
2022-06-26 18:49:37 +0300

    presence: no error logs

--- lua/plugins/presence.lua
+++ lua/plugins/presence.lua
@@ -1,5 +1,4 @@
 require('presence'):setup({
   buttons = { { label = 'View more...', url = 'https://youtu.be/dQw4w9WgXcQ' } },
-  log_level = 'error',
   neovim_image_text = 'vscode on steroids'
 })
