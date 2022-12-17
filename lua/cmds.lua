36dda54bb3f5a7c393ca282176757d888a1dc7d8
Nick Friday <nfriday@yandex.ru>
2022-12-17 05:44:56 +0300

    feat: blinking cursor

--- lua/cmds.lua
+++ lua/cmds.lua
@@ -10,4 +10,8 @@ au BufReadPost *.kbd set filetype=lisp
 au BufReadPost .env,.env.* set filetype=config
 
 command! W :w!
+
+set guicursor=n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50
+  \,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor
+  \,sm:block-blinkwait175-blinkoff150-blinkon175
 ]])
