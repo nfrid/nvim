490d06ae433784508c64a1cc708d057bd42062ab
Nick Friday <nfriday@yandex.ru>
2022-12-24 04:54:51 +0300

    format

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -295,14 +295,18 @@ local M = {
         FIX = {
           icon = ' ',
           color = 'error',
-          alt = { 'FIXME', 'BUG', 'FIXIT', 'FIX', 'ISSUE' }
+          alt = { 'FIXME', 'BUG', 'FIXIT', 'FIX', 'ISSUE' },
         },
         TODO = { icon = ' ', color = 'info' },
-        HACK = { icon = ' ', color = 'warning', alt = { 'FUCK', 'SHIT', 'BAD' } },
+        HACK = {
+          icon = ' ',
+          color = 'warning',
+          alt = { 'FUCK', 'SHIT', 'BAD' },
+        },
         WARN = { icon = ' ', color = 'warning', alt = { 'WARNING', 'XXX' } },
         PERF = { icon = ' ', alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE' } },
-        NOTE = { icon = ' ', color = 'hint', alt = { 'INFO' } }
-      }
+        NOTE = { icon = ' ', color = 'hint', alt = { 'INFO' } },
+      },
     },
     cmd = { 'TodoTelescope', 'TodoTrouble', 'TodoQuickFix', 'TodoLocList' },
   },
