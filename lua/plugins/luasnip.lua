8a05fcebeeaeba37cd0b42a2989a56e7555c92de
Nick Friday <nfriday@ya.ru>
2023-03-09 15:57:21 +0300

    feat(luasnip): prepare for custom snips

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: unused-local
 ---@type LazySpec
 local M = {
   'L3MON4D3/LuaSnip',
@@ -6,14 +7,37 @@ local M = {
 
 M.init = function()
   local mx = require('mapx')
-  mx.inoremap('<C-s>', function() require('luasnip').expand({}) end, 'Expand Snippet')
+  mx.inoremap('<C-s>', function()
+    require('luasnip').expand({})
+  end, 'Expand Snippet')
 end
 
 M.config = function()
   require('luasnip.loaders.from_snipmate').lazy_load()
-  local snip = require('luasnip')
-  snip.filetype_extend('javascriptreact', { 'javascript' })
-  snip.filetype_extend('typescriptreact', { 'typescript' })
+  local ls = require('luasnip')
+
+  local s = ls.snippet
+  local sn = ls.snippet_node
+  local t = ls.text_node
+  local i = ls.insert_node
+  local f = ls.function_node
+  local c = ls.choice_node
+  local d = ls.dynamic_node
+  local r = ls.restore_node
+  local l = require('luasnip.extras').lambda
+  local rep = require('luasnip.extras').rep
+  local p = require('luasnip.extras').partial
+  local m = require('luasnip.extras').match
+  local n = require('luasnip.extras').nonempty
+  local dl = require('luasnip.extras').dynamic_lambda
+  local fmt = require('luasnip.extras.fmt').fmt
+  local fmta = require('luasnip.extras.fmt').fmta
+  local types = require('luasnip.util.types')
+  local conds = require('luasnip.extras.conditions')
+  local conds_expand = require('luasnip.extras.conditions.expand')
+
+  ls.filetype_extend('javascriptreact', { 'javascript' })
+  ls.filetype_extend('typescriptreact', { 'typescript' })
 end
 
 return M
