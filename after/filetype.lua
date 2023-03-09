d94fdb43e0f62a2796e0b735044b7951b72f3aa1
Nick Friday <nfriday@ya.ru>
2023-03-09 15:59:45 +0300

    feat(filetype): media ft group

---
+++ after/filetype.lua
@@ -0,0 +1,12 @@
+local media_fts = { 'png', 'jpg', 'gif', 'mp4', 'webm', 'pdf' }
+local media_ft_group = vim.api.nvim_create_augroup('media_ft', {})
+
+for _, ft in pairs(media_fts) do
+  vim.api.nvim_create_autocmd('BufReadCmd', {
+    group = media_ft_group,
+    pattern = '*.' .. ft,
+    callback = function()
+      vim.cmd('setl ft=' .. ft)
+    end
+  })
+end
